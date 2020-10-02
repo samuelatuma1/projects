dd697070c05b23bd504f14b921e46321123b7a9a
samuelatuma1 <atumasaake@gmail.com>
2020-10-02 06:23:58 +0100

    changed some border radius settings

---
+++ proj2/try.py
@@ -0,0 +1,16 @@
+empty = []
+
+def fibon(x):
+    emp = []
+    for i in range(x):
+        if i == 0:
+            emp.append(1)
+        elif i == 1:
+            emp.append(1)
+        else:
+            emp.append(emp[i-1] + emp[i-2])
+
+
+
+
+
